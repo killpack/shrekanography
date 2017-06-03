5a2f3eaf9b1cc2b18909beb1d1c8410585fd77b9
Jordan Killpack <jordan@bignerdranch.com>
2017-06-03 19:04:40 -0400

    Add decoder test

---
+++ test/models/message/decoder_test.exs
@@ -0,0 +1,17 @@
+defmodule Shrekanography.Message.Decoder.Test do
+  alias Shrekanography.Message.Decoder
+  use ExUnit.Case, async: true
+
+  test "decode_pixels/2 undoes Encoder.encode_pixels/2" do
+    {:ok, png} = Imagineer.load("priv/shreks/shrek1.png")
+    message = """
+      Here's a pretty long message. Nice.
+      It's got unicode characters, too. よし！
+      Hooray for messages!
+      """
+    encoded_pixels = Shrekanography.Message.Encoder.encode_pixels(message, png.pixels)
+    decoded_message = Decoder.decode_pixels(encoded_pixels)
+
+    assert decoded_message === message
+  end
+end
