0de76921b7484e0a57931dcea97a9461620018b4
Jordan Killpack <jordan@bignerdranch.com>
2017-06-03 13:08:52 -0400

    Change log level

--- config/prod.exs
+++ config/prod.exs
@@ -19,7 +19,8 @@ force_ssl: [rewrite_on: [:x_forwarded_proto]],
   secret_key_base: System.get_env("SECRET_KEY_BASE")
 
 # Do not print debug messages in production
-config :logger, level: :info
+# config :logger, level: :info
+config :logger, level: :debug
 
 # ## SSL Support
 #
