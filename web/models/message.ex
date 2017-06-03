2cd45bc1875d4b7cdd009af6d9a22bcae0741ce7
Jordan Killpack <jordan@bignerdranch.com>
2017-06-03 11:52:47 -0400

    Decompose message encoding

--- web/models/message.ex
+++ web/models/message.ex
@@ -5,13 +5,8 @@ defmodule Shrekanography.Message do
   defstruct [body: nil]
   ## TODO message length- encode in first pixel
 
-  def encode(message = %Message{}, shrek_path \\ "priv/shreks/shrek1.png") do
-    # Let's only support RGBa images for now
-    {:ok, png = %Imagineer.Image.PNG{color_format: :rgb_alpha}} = Imagineer.load(shrek_path)
-
-    encoded_pixels = do_encode(message.body, png.pixels, [], [])
-    updated_png = %Imagineer.Image.PNG{png | pixels: encoded_pixels}
-    Imagineer.Image.PNG.to_binary(updated_png)
+  def encode(message) do
+    Message.Encoder.encode(message)
   end
 
   def decode(shrek_path \\ "priv/shreks/secret_shrek.png") do
@@ -19,55 +14,6 @@ defmodule Shrekanography.Message do
 
     decoded_message = do_decode(png.pixels)
   end
-
-
-  defp do_encode(message, [[first_pixel | remaining_row_pixels] | remaining_rows], [], []) do
-    # Encode the length of the message in the first pixel
-    message_length = byte_size(message)
-    encoded_pixel = encode_pixel(first_pixel, message_length)
-    do_encode(message, [remaining_row_pixels | remaining_rows], [encoded_pixel], [])
-  end
-  defp do_encode(message, [[] | [next_row | remaining_rows]], in_progress_row, finished_rows) do
-    # This row is done- let's add it to the pile and move on to the next row
-    IO.puts("ran out of rows")
-    do_encode(message, next_row, [], [in_progress_row | finished_rows])
-  end
-  defp do_encode(<<>>, [remaining_row_pixels | remaining_rows], in_progress_row, finished_rows) do
-    # No more characters in the message- finish up
-    current_row = Enum.reverse(in_progress_row) ++ remaining_row_pixels
-    encoded_rows = Enum.reverse(finished_rows) ++ current_row
-    [encoded_rows] ++ remaining_rows
-  end
-  defp do_encode(<<message_byte::integer, remaining_message::binary>>,
-                 [[pixel | remaining_row_pixels] | remaining_rows],
-                 in_progress_row,
-                 finished_rows) do
-    encoded_pixel = encode_pixel(pixel, message_byte)
-
-    do_encode(remaining_message,
-              [remaining_row_pixels | remaining_rows],
-              [encoded_pixel | in_progress_row],
-              finished_rows)
-  end
-
-  defp encode_pixel(pixel, message_byte) do
-    red_bits   = message_byte >>> 6 |> rem(4) # most significant bits...
-    green_bits = message_byte >>> 4 |> rem(4)
-    blue_bits  = message_byte >>> 2 |> rem(4)
-    alpha_bits = message_byte       |> rem(4) # ... down to the least.
-
-    { set_least_significant_bits(elem(pixel, 0), red_bits),
-      set_least_significant_bits(elem(pixel, 1), green_bits),
-      set_least_significant_bits(elem(pixel, 2), blue_bits),
-      set_least_significant_bits(elem(pixel, 3), alpha_bits)}
-  end
-
-  defp set_least_significant_bits(byte, bits_to_set) do
-    byte
-      &&& 0b11111100 # Clear out the two least significant bits...
-      ||| bits_to_set # and replace them with the desired bits.
-  end
-
 
   defp do_decode(pixels) do
     [first_pixel | remaining_pixels] = List.flatten(pixels)
---
+++ web/models/message/encoder.ex
@@ -0,0 +1,90 @@
+defmodule Shrekanography.Message.Encoder do
+  use Bitwise
+
+  def encode(message = %Shrekanography.Message{}, shrek_path \\ "priv/shreks/shrek1.png") do
+    # Let's only support RGBa images for now
+    {:ok, png = %Imagineer.Image.PNG{color_format: :rgb_alpha}} = Imagineer.load(shrek_path)
+
+    encoded_pixels = encode_pixels(message.body, png.pixels)
+    updated_png = %Imagineer.Image.PNG{png | pixels: encoded_pixels}
+    Imagineer.Image.PNG.to_binary(updated_png)
+  end
+
+  def encode_pixels(message_body, png_pixels) do
+    encode_pixels(message_body, png_pixels, [], [])
+  end
+
+  # TODO handle case when the message is longer than the number of pixels
+
+  # Case: we haven't processed any message characters yet
+  defp encode_pixels(message,
+                     _remaining_pixels = [[first_pixel | remaining_row_pixels] | remaining_rows],
+                     _working_row = [],
+                     finished_rows = []) do
+
+    # encode the length of the message into the first pixel
+    message_length = byte_size(message)
+    encoded_pixel = encode_pixel(first_pixel, message_length)
+
+    encode_pixels(message,
+                  [remaining_row_pixels | remaining_rows],
+                  [encoded_pixel],
+                  finished_rows)
+  end
+
+  # Case: there aren't any pixels left to process in the current row
+  defp encode_pixels(remaining_message,
+                     _remaining_pixels = [[] | [next_row | remaining_rows]],
+                     working_row,
+                     finished_rows) do
+    # This row is done- let's add it to the pile of processed rows and move on to the next row
+    encode_pixels(remaining_message, next_row, [], [working_row | finished_rows])
+  end
+
+  # Case: no more characters in the message
+  defp encode_pixels(_remaining_message = <<>>,
+                     _remaining_pixels = [remaining_row_pixels | remaining_rows],
+                     working_row,
+                     finished_rows) do
+    # Smush our placeholders together and return the encoded pixels
+    current_row = Enum.reverse(working_row) ++ remaining_row_pixels
+    encoded_rows = Enum.reverse(finished_rows) ++ current_row
+
+    [encoded_rows] ++ remaining_rows
+  end
+
+  # General case: encode a message character into a pixel
+  defp encode_pixels(_message = <<message_byte::integer, remaining_message::binary>>,
+                     _remaining_pixels = [[pixel | remaining_row_pixels] | remaining_rows],
+                     working_row,
+                     finished_rows) do
+    encoded_pixel = encode_pixel(pixel, message_byte)
+
+    encode_pixels(remaining_message,
+                  [remaining_row_pixels | remaining_rows],
+                  [encoded_pixel | working_row],
+                  finished_rows)
+  end
+
+  defp encode_pixel(pixel, message_byte) do
+    # Stash message data in the two least significant bits of each channel.
+
+    # Split up the message byte into four two-bit chunks:
+    red_bits   = message_byte >>> 6 |> rem(4) # most significant bits...
+    green_bits = message_byte >>> 4 |> rem(4)
+    blue_bits  = message_byte >>> 2 |> rem(4)
+    alpha_bits = message_byte       |> rem(4) # ... down to the least.
+
+    # Then set the least significant bits of the pixel accordingly
+    { set_least_significant_bits(elem(pixel, 0), red_bits),
+      set_least_significant_bits(elem(pixel, 1), green_bits),
+      set_least_significant_bits(elem(pixel, 2), blue_bits),
+      set_least_significant_bits(elem(pixel, 3), alpha_bits) }
+  end
+
+  defp set_least_significant_bits(byte, bits_to_set) do
+    byte
+      &&& 0b11111100 # Clear out the two least significant bits...
+      ||| bits_to_set # and replace them with the desired bits.
+  end
+end
