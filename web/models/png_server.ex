8cc8f793394b8b12247952106f85d7fbe44a55bd
Jordan Killpack <jordan@bignerdranch.com>
2017-06-07 18:21:15 -0400

    Increase boot speed by loading PNGs at compile time
    ...at a cost of a slower compile. But this improves the experience on
    Heroku where my poor cheapy dyno sleeps all the time.

--- web/models/png_server.ex
+++ web/models/png_server.ex
@@ -1,29 +1,22 @@
 defmodule Shrekanography.PngServer do
+  @shrek_paths Path.wildcard("priv/shreks/*.png")
+  @pngs (for shrek_path <- @shrek_paths do
+    {:ok, png} = Imagineer.load(shrek_path)
+    shrek_filename = Path.basename(shrek_path)
 
-  def start_link do
-    shrek_paths = Path.wildcard("priv/shreks/*.png")
+    {shrek_filename, png}
+  end |> Map.new)
 
-    pngs = for shrek_path <- shrek_paths do
-      {:ok, png} = Imagineer.load(shrek_path)
-      shrek_filename = Path.basename(shrek_path)
-
-      {shrek_filename, png}
-    end |> Map.new
-
-    Agent.start_link(fn -> pngs end, name: __MODULE__)
-  end
 
   def fetch_png(""), do: fetch_random_png()
   def fetch_png(nil), do: fetch_random_png()
 
   def fetch_png(shrek_filename) do
-    Agent.get(__MODULE__, fn pngs -> {shrek_filename, pngs[shrek_filename]} end)
+    {shrek_filename, @pngs[shrek_filename]}
   end
 
   def fetch_random_png do
-    Agent.get(__MODULE__, fn pngs ->
-      random_key = Map.keys(pngs) |> Enum.random
-      {random_key, pngs[random_key]}
-    end)
+    random_key = Map.keys(@pngs) |> Enum.random
+    {random_key, @pngs[random_key]}
   end
 end
